44576285d9c538ff305eb12a0512c0d8f04e0c8b
Francesco <f.coppola.mos80@gmail.com>
2015-10-01 16:34:06 +0200

    test for modals without default_options

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -1,8 +1,8 @@
 module ModalHelper
-  
-  def default_options
-    return {:id => 'modal', :size => '', :show_close => true, :dismiss => true}
-  end
+
+#  def default_options
+#    return {:id => 'modal', :size => '', :show_close => true, :dismiss => true}
+#  end
 
   #modals have a header, a body, a footer for options.
   def modal_dialog(options = {}, &block)
@@ -20,12 +20,12 @@ module ModalHelper
 
   def modal_header(options, &block)
     content_tag :div, :class => 'modal-header' do
-      if options[:show_close] 
+      if options[:show_close]
         close_button(options[:dismiss]) +
         content_tag(:h4, options[:title], :class => 'modal-title', &block)
       else
         content_tag(:h4, options[:title], :class => 'modal-title', &block)
-      end   
+      end
     end
   end
 
@@ -38,7 +38,7 @@ module ModalHelper
   end
 
   def close_button(dismiss)
-    #It doesn't seem to like content_tag, so we do this instead.    
+    #It doesn't seem to like content_tag, so we do this instead.
     raw("<button class=\"close\" data-dismiss=\"#{dismiss}\" aria-hidden=\"true\">&times;</button>")
   end
 
